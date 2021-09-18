a45aa0903097ad2a7adb766cdccc67e2cab060d6
Arthur Freitas <atpfreitas@me.com>
2021-09-18 21:21:31 +0000

    translates twitter embed error message

--- _plugins/embed_tweets.rb
+++ _plugins/embed_tweets.rb
@@ -4,15 +4,16 @@ class TweetEmbedGenerator < Jekyll::Generator
     return if !site.config["embed_tweets"]
 
     all_notes = site.collections['notes'].docs
+    all_posts = site.posts
     all_pages = site.pages
-    all_docs = all_notes + all_pages
+    all_docs = all_notes + all_posts + all_pages
 
     all_docs.each do |current_note|
       current_note.content.gsub!(
         /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)$/i,
         <<~HTML
           <blockquote class="twitter-tweet">
-           This tweet could not be embedded. <a href="#{'\0'}">View it on Twitter instead.</a>
+           Esse tuíte não pode ser mostrado. <a href="#{'\0'}">Tente vê-lo no Twitter.</a>
           </blockquote>
           <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         HTML
