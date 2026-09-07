03ac97cb590f29c998e7a7f1c611d25800fb44b5
Arthur Freitas <atpfreitas@me.com>
2026-09-07 18:50:43 -0300

    fix feed links

--- _plugins/feed_link_patch.rb
+++ _plugins/feed_link_patch.rb
@@ -1,14 +1,9 @@
 require "jekyll-feed"
 
 module JekyllFeedLinkPatch
-  ORIGINAL = '<link href="{{ post.url | absolute_url }}" rel="alternate" type="text/html" title="{{ post_title }}" />'
-
   PATCHED = <<~LIQUID.strip
     {% if post.external_url %}
-    <link href="{{ post.external_url }}" rel="alternate" type="text/html" title="{{ post_title }}" />
-    <link href="{{ post.url | absolute_url }}" rel="related" type="text/html" title="Permalink" />
-    {% else %}
-    #{ORIGINAL}
+    <link href="{{ post.external_url }}" rel="related" type="text/html" title="Permalink" />
     {% endif %}
   LIQUID
 
